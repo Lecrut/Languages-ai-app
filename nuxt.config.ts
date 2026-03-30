6437eab285397f084dd9a9b4cb857fb10243ef92
Lecrut <filip.lecrut12@gmail.com>
2026-03-30 19:37:27 +0200

    add firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,5 +18,17 @@ export default defineNuxtConfig({
     '@nuxtjs/emotion',
     '@nuxtjs/eslint-module',
     '@nuxtjs/tailwindcss'
-  ]
+  ],
+
+    runtimeConfig: {
+    public: {
+      apiKey: import.meta.env.apiKey,
+      authDomain: import.meta.env.authDomain,
+      projectId: import.meta.env.projectId,
+      storageBucket: import.meta.env.storageBucket,
+      messagingSenderId: import.meta.env.messagingSenderId,
+      appId: import.meta.env.appId,
+      measurementId: import.meta.env.measurementId,
+    },
+  },
 })
